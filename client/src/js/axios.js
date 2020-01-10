c8e296889e286f10fe880b9798e5f82c8f0c60cb
Vilzuge <ville.martas@hotmail.com>
2020-01-10 19:46:29 +0200

    Update axios.js

--- client/src/js/axios.js
+++ client/src/js/axios.js
@@ -3,7 +3,12 @@ import axios from "axios";
 // This axios file will be imported instead of default axios
 // and it will user the backend url as the baseURL and make the code cleaner
 const instance = axios.create({
+<<<<<<< Updated upstream
     baseURL: "http://localhost:8080/"
+=======
+  //baseURL: "http://192.168.1.37:8080/"
+  baseURL: "http://localhost:8080/"
+>>>>>>> Stashed changes
 });
 
 export default instance;
