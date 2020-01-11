14e37760db903ab5a10ef6da4709077ce1bc9da3
Hugo <hugo@deimos.local>
2020-01-11 22:40:32 +0200

    add lobby check on join

--- client/src/components/mobile/MainMenu.js
+++ client/src/components/mobile/MainMenu.js
@@ -1,22 +1,58 @@
 import React, { Component } from "react";
 import {Redirect} from "react-router-dom";
+import axios from "../../js/axios"
 import "../../styles.css";
+import {GameContext} from "../GameContext"
 
 // Mobile view
 class MainMenu extends Component {
   constructor(props) {
     super(props);
-    this.handleSubmit = this.handleSubmit.bind(this);
+
     this.state = { goToNextPage: false };
+    this.info = {username: '', gameid: ''};
+
+    this.onChangeRoomcode = this.onChangeRoomcode.bind(this);
+    this.onChangeUsername = this.onChangeUsername.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  static contextType = GameContext
 
-  handleSubmit() {
-    this.setState({ goToNextPage: true});
+  handleSubmit(event) {
+    event.preventDefault();
+    this.joinLobby(this.context);
+    //  let lobbyStatus = joinLobby(info.lobbyid, info.username);
+    //  res.status( 200 ).json({ lobbyStatus });
+  }
+
+  // Update the login info variable when something is changed on the field
+  onChangeRoomcode(event) {
+    this.info.gameid = event.target.value;
+  }
+
+  // Update the login info variable when something is changed on the field
+  onChangeUsername(event) {
+      this.info.username = event.target.value;
+  }
+
+  // Try to log in with given input
+  async joinLobby(context) {
+    const info =   {
+        username: this.info.username,
+        lobbyid: this.info.gameid
+    };
+    const { data } = await axios.post("/lobby/join", { info: info });
+
+    const lobbyStatus = data.lobbyStatus;
+    if(lobbyStatus === 1) {
+      this.setState({ goToNextPage: true});
+    }
   }
 
   render() {
     if(this.state.goToNextPage) return <Redirect to='/wait' />
+
     const headerStyle = {
       fontSize: "6vmin",
       fontFamily: "Bangers"
@@ -31,23 +67,23 @@ class MainMenu extends Component {
             <div className="row">
               <form
                 className="col s12"
-                onChange={this.handleUsername}
+                onChange={this.onChangeRoomcode}
                 onSubmit={this.handleSubmit}
               >
                 <div className="row">
                   <div className="input-field col s12">
-                    <input id="username1" type="text" className="validate" />
-                    <label htmlFor="username1" className="active">
+                    <input id="roomcode" type="text" className="validate" />
+                    <label htmlFor="roomcode" className="active">
                       Enter the Room Code
                     </label>
                   </div>
                 </div>
               </form>
-              <form className="col s12">
+              <form className="col s12" onChange={this.onChangeUsername} onSubmit={this.handleSubmit}>
                 <div className="row">
                   <div className="input-field col s12">
-                    <input id="password" type="text" className="validate" />
-                    <label htmlFor="password" className="active">
+                    <input id="usernamefield" type="text" className="validate" />
+                    <label htmlFor="usernamefield" className="active">
                       Choose Your Username
                     </label>
                   </div>
@@ -59,7 +95,7 @@ class MainMenu extends Component {
                 className="btn-large waves-effect waves-light deep-orange darken-1"
                 onClick={this.handleSubmit}
               >
-                <i class="material-icons right">arrow_forward</i>
+                <i className="material-icons right">arrow_forward</i>
                 Join Lobby
               </div>
             </div>
